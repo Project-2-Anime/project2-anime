40cbcb5b68fcdd7b2b8b13f5f75f614c898b2249
jingru <nicoxu1000@gmail.com>
2022-07-20 18:50:30 -0400

    get our facts portion of Javascript working

--- scripts.js
+++ scripts.js
@@ -22,8 +22,19 @@
 const app = {};
 //7.  Created a new array to hold our images
 app.imgArray = [];
+// 18. create a new array to hold the image ID for alt tags 
+app.altArray = [];
 // 6. variable to hold our DOM selected element
 app.animePicture = document.querySelector('#animePicture')
+
+// variable to hold the DOM selected li .flexFacts
+app.animeFacts = document.querySelector('.flexFact div')
+
+// WHERE WE LEFT OUT, TRYING TO TARGET OUR FACTS
+// 17. Create a variable to target the user selection value from our options that only look at the string
+
+
+
 // 2.Init Method
 app.init = () => {
     //4. testing to see if init works
@@ -35,17 +46,20 @@ app.init = () => {
 // START OUR FACTS AJAX CALL
 
 // 16. Create a method to hold our AJAX call with a different endpoint (facts)
-app.facts = () => {
-    fetch(`https://anime-facts-rest-api.herokuapp.com/api/v1`)
+app.facts = (userValue) => {
+    //21. passed a parameter into our API as the endpoint 
+    fetch(`https://anime-facts-rest-api.herokuapp.com/api/v1/${userValue}`)
         .then(function (resp) {
             return resp.json();
         })
         .then(function (result) {
-            const newArray = result.data;
-            // console.log(newArray)
-        
+            const factArray = result.data[0].fact;
+            // console.log(factArray);
             // append this to our empty div
+
+            app.displayFacts(factArray)
         })
+
 }
 
 // END OUR FACTS AJAX CALL
@@ -61,16 +75,26 @@ app.getImage = function () {
         .then(function (jsonData) {
             jsonData.data.forEach(item => {
                 app.imgArray.push(item.anime_img)
+                // 19. pushed our anime_name into our new array app.altArray
+                app.altArray.push(item.anime_name)
             })
         })
 }
 
 // 9. Created a method to display our images and append to the empty div.
-app.displayImage = function (imgSelected) {
+app.displayImage = function (imgSelected,altSource) {
 //   10. Made a variable that hold our created img element
     const img = document.createElement('img');
     img.src = imgSelected;
+    img.alt = altSource;
     app.animePicture.appendChild(img);
+}
+// 23. Created a method to display our anime facts and append to the div
+app.displayFacts = function(factSelected){
+    // made a varible to hold our created p element
+    const factEl = document.createElement('p');
+    factEl.innerText = factSelected;
+    app.animeFacts.append(factEl);
 }
 
 // 11. Created an event listener of change onto our select dropdown
@@ -80,22 +104,23 @@ app.events = function () {
         const userSelection = parseInt(this.value);
         // 13. created a variable to hold the selected value from our newly created array
         const userPicture = app.imgArray[userSelection];
+        // 20. created a variable to hold the user selected value from the app.altArray array 
+        const userAlt = app.altArray[userSelection];
 
         // WHERE WE LEFT OUT, TRYING TO TARGET OUR FACTS
         // 17. Create a variable to target the user selection value from our options that only look at the string
-
         const valueOne = document.querySelector('#anime').value.split(',')[1];
-        console.log(valueOne)
-
-        // const userFact = contains(this.value);
-        // console.log(userFact)
-
 
 
         // 14. make sure our empty div is cleared before each call
         app.animePicture.innerHTML = '';
         // 15.called our method that displays the image and pass our user selection into it. 
-        app.displayImage(userPicture);
+        app.displayImage(userPicture,userAlt);
+        // 22.called the facts API and passed the valueOne as the argument 
+        app.animeFacts.innerHTML = '';
+        app.facts(valueOne);
+
+
     })
 }
 // 3. Called our init method
